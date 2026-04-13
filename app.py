551dbdc59ff93712ad5da5b62af9d6f135310043
mahmed abdallh <medotatch124@gmail.com>
2026-04-13 23:28:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ import io
 
 # ------------------------------- الإعدادات الثابتة -------------------------------
 APP_CONFIG = {
-    "APP_TITLE": "نظام إدارة الصيانة - CMMS",
+    "APP_TITLE": "coats - CMMS",
     "APP_ICON": "🏭",
-    "REPO_NAME": "mahmedabdallh123/Elqds",
+    "REPO_NAME": "mahmedabdallh123/coats",
     "BRANCH": "main",
     "FILE_PATH": "3.xlsx",
     "LOCAL_FILE": "3.xlsx",
@@ -62,8 +62,8 @@ IMAGES_FOLDER = APP_CONFIG["IMAGES_FOLDER"]
 EQUIPMENT_CONFIG_FILE = "equipment_config.json"
 
 GITHUB_EXCEL_URL = f"https://github.com/{APP_CONFIG['REPO_NAME'].split('/')[0]}/{APP_CONFIG['REPO_NAME'].split('/')[1]}/raw/{APP_CONFIG['BRANCH']}/{APP_CONFIG['FILE_PATH']}"
-GITHUB_USERS_URL = "https://raw.githubusercontent.com/mahmedabdallh123/Elqds/refs/heads/main/users.json"
-GITHUB_REPO_USERS = "mahmedabdallh123/Elqds"
+GITHUB_USERS_URL = "https://raw.githubusercontent.com/mahmedabdallh123/coats/refs/heads/main/users.json"
+GITHUB_REPO_USERS = "mahmedabdallh123/coats"
 
 # ------------------------------- دوال قطع الغيار -------------------------------
 def load_spare_parts():
